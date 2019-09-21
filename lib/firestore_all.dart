50c6eb3858c6d9435a16712347eabc6e8c5fc063
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-09-21 13:29:24 +0200

    fix exporting web firestore

--- lib/firestore_all.dart
+++ lib/firestore_all.dart
@@ -1,3 +1,3 @@
 export 'src/firestore_interface.dart'
   if (dart.library.io) 'src/firestore_mobile.dart'
-  if (dart.library.html) 'src/firestore_js.dart';
+  if (dart.library.html) 'src/firestore_web.dart';
