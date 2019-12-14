9f304d55b1ad891fbb9be51f622802b209965561
Stefan Rusek <stefan@rusek.org>
2019-12-13 20:46:20 -0500

    oops had conditions swapped

--- lib/firestore_all.dart
+++ lib/firestore_all.dart
@@ -3,7 +3,7 @@ export 'src/firestore_interface.dart'
     if (dart.library.io) 'src/firestore_mobile.dart';
 
 export 'package:firebase/firestore.dart'
-    if (dart.library.html) 'package:cloud_firestore/cloud_firestore.dart'
-    if (dart.library.io) 'package:firebase/firestore.dart' show FieldValue;
+    if (dart.library.io) 'package:cloud_firestore/cloud_firestore.dart'
+    if (dart.library.html) 'package:firebase/firestore.dart' show FieldValue;
 
 export 'src/firestore_interface.dart' show Blob, DocumentChangeType, GeoPoint;
